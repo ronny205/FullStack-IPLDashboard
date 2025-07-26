4717f485a48e95c55cc30b66c78a8550f988ae57
ronny205 <rohanaiwale99@gmail.com>
2025-07-26 21:54:50 +0530

    FullStack IPL Dashboard Backend Setup

---
+++ ipl-dashboard/src/test/java/com/ronnie/ipl_dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronnie.ipl_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
